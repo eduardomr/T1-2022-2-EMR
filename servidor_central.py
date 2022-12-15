2212a1ff8d8ccb1849d936554487efa3df6a240f
Eduardo <emaiarezende@gmail.com>
2022-12-14 23:17:06 -0300

    configurando conexão com sala 1

--- servidor_central.py
+++ servidor_central.py
@@ -1,18 +1,124 @@
 from socket import *
 import threading
 import time
+import json
 
 
-host = '127.0.0.1'
-port = 5000
-servidor_central = socket(AF_INET, SOCK_STREAM)
-destino = (host,port)
-servidor_central.connect(destino)
+def configura():
+    global ip_distribuido1
+    global ip_distribuido2
+    
+    with open('configuracao_sala_01.json', 'r') as f:
+        data = json.load(f)
+        ip_distribuido1 = data["ip_servidor_distribuido"]
+        porta1 = data["porta_servidor_distribuido"]
+    # Closing file
+    f.close()
+
+    with open('configuracao_sala_02.json', 'r') as f:
+        data = json.load(f)
+        ip_distribuido2 = data["ip_servidor_distribuido"]
+        porta2 = data["porta_servidor_distribuido"]
+    f.close()
+
+    servidor_central1 = socket(AF_INET, SOCK_STREAM)
+    servidor_central2 = socket(AF_INET, SOCK_STREAM)
+    destino1 = (ip_distribuido1,porta1)
+    destino2 = (ip_distribuido2,porta2)
+    servidor_central1.connect(destino1)
+    servidor_central2.connect(destino2)
 
 
-while(1):
-    mensagem = input()
-    servidor_central.send(bytes(mensagem,"utf8"))
-    if mensagem == "x":
-        tcp.close()
-        break
+
+
+
+
+
+
+
+
+
+def menu_sala1():
+    global servidor_central1
+    print("SALA 1")
+    print("Escolha uma das opções abaixo:")
+    print("1 - Ligar Luz 1")
+    print("2 - Desligar Luz 1")
+    print("3 - Ligar Luz 2")
+    print("4 - Desligar Luz 2")
+    print("5 - Ligar Ar Condicionado")
+    print("6 - Desligar Ar Condicionado")
+    print("7 - Sistema de Alarme")
+    opt = int(input())
+    if opt == 1:
+        servidor_central1.send(bytes("L01","utf8"))
+    elif opt == 2:
+        servidor_central1.send(bytes("DL01","utf8"))
+    elif opt == 3:
+        servidor_central1.send(bytes("L02","utf8"))
+    elif opt == 4:
+        servidor_central1.send(bytes("DL02","utf8"))
+    elif opt == 5:
+        servidor_central1.send(bytes("AC","utf8"))
+    elif opt == 6:
+        servidor_central1.send(bytes("DAC","utf8"))
+    elif opt == 7:
+        servidor_central1.send(bytes("AL","utf8"))
+    else:
+        print("Opção inválida!")
+        menu_sala1()
+
+
+def menu_sala2():
+    global servidor_central2
+    print("SALA 2")
+    print("Escolha uma das opções abaixo:")
+    print("1 - Ligar Luz 1")
+    print("2 - Desligar Luz 1")
+    print("3 - Ligar Luz 2")
+    print("4 - Desligar Luz 2")
+    print("5 - Ligar Ar Condicionado")
+    print("6 - Desligar Ar Condicionado")
+    print("7 - Sistema de Alarme")
+    opt = int(input())
+    if opt == 1:
+        servidor_central2.send(bytes("L01","utf8"))
+    elif opt == 2:
+        servidor_central2.send(bytes("DL01","utf8"))
+    elif opt == 3:
+        servidor_central2.send(bytes("L02","utf8"))
+    elif opt == 4:
+        servidor_central2.send(bytes("DL02","utf8"))
+    elif opt == 5:
+        servidor_central2.send(bytes("AC","utf8"))
+    elif opt == 6:
+        servidor_central2.send(bytes("DAC","utf8"))
+    elif opt == 7:
+        servidor_central2.send(bytes("AL","utf8"))
+    else:
+        print("Opção inválida!")
+        menu_sala1()
+
+def apresenta_menu():
+    opcao = 0
+    print("Bem vindo ao sistema de automação de predial!")
+    print("Escolha uma das opções abaixo:")
+    print("1 - Controlar Sala 1")
+    print("2 - Controlar Sala 2")
+    print("3 - Controlar salas em conjunto ")
+    print("4 - Sair do sistema")
+    opcao = int(input())
+    if opcao == 1:
+        menu_sala1()
+    elif opcao == 2:
+        menu_sala2()
+        
+
+
+def main():
+    configura()
+    apresenta_menu()
+
+
+if __name__ == "__main__":
+    main()
--- servidor_distribuido.py
+++ servidor_distribuido.py
@@ -1,8 +1,10 @@
 import RPi.GPIO as GPIO
 from socket import *
+import json
 
-host = '127.0.0.1'
-port = 5000
+
+host = ''
+port = 10561
 servidor_distribuido = socket(AF_INET, SOCK_STREAM)
 servidor_distribuido.bind((host, port))
 servidor_distribuido.listen()
@@ -14,45 +16,73 @@ GPIO.setmode(GPIO.BCM)
 
 
 # Configuração das pinos GPIOs------------------------------------------------------
-L_01 = 26
-L_02 = 19
-AC = 13
-PR = 6
-AL_BZ = 5
-SPres = 0
-SFum = 11
-SJan = 9
-SPor = 10
-SC_IN = 22
-SC_OUT = 27
-DHT22 = 18
+L_01 = -1
+L_02 = -1
+AC = -1
+PR = -1
+AL_BZ = -1
+SPres = -1
+SFum = -1
+SJan = -1
+SPor = -1
+SC_IN = -1
+SC_OUT = -1
+DHT22 = -1
 # ------------------------------------------------------------------------------------
+def configuracao():
+    global L_01
+    global L_02
+    global AC
+    global PR
+    global AL_BZ
+    global SPres
+    global SFum
+    global SJan
+    global SPor
+    global SC_IN
+    global SC_OUT
+    global DHT22
+    with open('configuracao_sala_01.json', 'r') as f:
+        data = json.load(f)
+        L_01 = data["L_01"]
+        L_02 = data["L_02"]
+        AC = data["AC"]
+        PR = data["PR"]
+        AL_BZ = data["AL_BZ"]
+        SPres = data["SPres"]
+        SFum = data["SFum"]
+        SJan = data["SJan"]
+        SPor = data["SPor"]
+        SC_IN = data["SC_IN"]
+        SC_OUT = data["SC_OUT"]
+        DHT22 = data["DHT22"]
+    f.close()
 
-# Configuração dos pinos GPIOs (SAIDA/ENTRADA)
+    # Configuração dos pinos GPIOs (SAIDA/ENTRADA)
 
-GPIO.setup(L_01, GPIO.OUT)
-GPIO.setup(L_02, GPIO.OUT)
-GPIO.setup(AC, GPIO.OUT)
-GPIO.setup(PR, GPIO.OUT)
-GPIO.setup(AL_BZ, GPIO.OUT)
+    GPIO.setup(L_01, GPIO.OUT)
+    GPIO.setup(L_02, GPIO.OUT)
+    GPIO.setup(AC, GPIO.OUT)
+    GPIO.setup(PR, GPIO.OUT)
+    GPIO.setup(AL_BZ, GPIO.OUT)
 
 
-GPIO.setup(SJan, GPIO.IN)
-GPIO.setup(SPres, GPIO.IN)
-GPIO.setup(SFum, GPIO.IN)
-GPIO.setup(SPor, GPIO.IN)
-GPIO.setup(SC_IN, GPIO.IN)
-GPIO.setup(SC_OUT, GPIO.IN)
-# ------------------------------------------------------------------------------------
+    GPIO.setup(SJan, GPIO.IN)
+    GPIO.setup(SPres, GPIO.IN)
+    GPIO.setup(SFum, GPIO.IN)
+    GPIO.setup(SPor, GPIO.IN)
+    GPIO.setup(SC_IN, GPIO.IN)
+    GPIO.setup(SC_OUT, GPIO.IN)
+    # ------------------------------------------------------------------------------------
 
 
 # Configuração dos pinos GPIOs (DETECÇÃO DE EVENTOS)
-GPIO.add_event_detect(SJan, GPIO.BOTH)
-GPIO.add_event_detect(SPor, GPIO.BOTH)
-GPIO.add_event_detect(SPres, GPIO.BOTH)
-GPIO.add_event_detect(SFum, GPIO.RISING)
-GPIO.add_event_detect(SC_IN, GPIO.RISING)
-GPIO.add_event_detect(SC_OUT, GPIO.RISING)
+    GPIO.add_event_detect(SJan, GPIO.BOTH)
+    GPIO.add_event_detect(SPor, GPIO.BOTH)
+    GPIO.add_event_detect(SPres, GPIO.BOTH)
+    GPIO.add_event_detect(SFum, GPIO.RISING)
+    GPIO.add_event_detect(SC_IN, GPIO.RISING)
+    GPIO.add_event_detect(SC_OUT, GPIO.RISING)
 # ------------------------------------------------------------------------------------
 
 
@@ -152,33 +182,37 @@ def contadorPessoas():
         print(pessoas)
 
 # ------------------------------------------------------------------------------------
+def main():
+    configuracao()
+    while True:
+        msg = conexao.recv(1024)
+        if not msg:
+            break
+        print("recebido:", msg.decode())
+        conexao.send(msg)
 
-while(1):
-    msg = conexao.recv(1024)
-    if not msg:
-        break
-    print("recebido:", msg.decode())
-    conexao.send(msg)
+        if msg.decode() == "L01":
+            ligaLuz01()
+        elif msg.decode() == "DL01":
+            desligaLuz01()
+        elif msg.decode() == "L02":
+            ligaLuz02()
+        elif msg.decode() == "DL02":
+            desligaLuz02()
+        elif msg.decode() == "AC":
+            ligaAC()
+        elif msg.decode() == "DAC":
+            desligaAC()
+        elif msg.decode() == "PR":
+            ligaPR()
+        elif msg.decode() == "DPR":
+            desligaPR()
+        elif msg.decode() == "x":
+            servidor_distribuido.close()
+            break
+        
 
-    if msg.decode() == "l1":
-        ligaLuz01()
-    elif msg.decode() == "dl1":
-        desligaLuz01()
-    elif msg.decode() == "l2":
-        ligaLuz02()
-    elif msg.decode() == "dl2":
-        desligaLuz02()
-    elif msg.decode() == "lac":
-        ligaAC()
-    elif msg.decode() == "dlac":
-        desligaAC()
-    elif msg.decode() == "lp":
-        ligaPR()
-    elif msg.decode() == "dlp":
-        desligaPR()
-    elif msg.decode() == "x":
-        tcp.close()
-        break
+    
 
    
     
